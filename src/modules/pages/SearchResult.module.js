53224534bf42a627b1d0a3ab66548ac80f3db5bf
heartless-unicorn <pavlovamofficial@gmail.com>
2023-03-30 20:55:09 +0200

    added search

--- src/modules/pages/SearchResult.module.js
+++ src/modules/pages/SearchResult.module.js
@@ -1,19 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Grid from "../pages/breeds/Grid.module";
 
 export default function SearchResult() {
   let { breed } = useParams();
+  const [searchResult, setsearchResult] = useState([]);
   useEffect(() => {
     fetch("https://api.thecatapi.com/v1/breeds/")
       .then((response) => response.json())
       .then((data) => {
         data.map((el) => {
-          if (el.name.toLowerCase() === breed) {
-            console.log("match");
+          if (el.name.toLowerCase().includes(breed)) {
+            setsearchResult((cur) => [...cur, el.reference_image_id]);
           }
         });
-        console.log(data);
       });
-  });
-  return <div>Here i search result</div>;
+  }, []);
+  console.log(searchResult);
+  return (
+    <div>
+      {searchResult.length > 0 ? (
+        <Grid data={searchResult} />
+      ) : (
+        <p>"No items found"</p>
+      )}
+    </div>
+  );
 }
